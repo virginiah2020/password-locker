11819291a1765f39585e099205f931f4335a775a
virginiah2020 <vancyvy254@gmail.com>
2020-06-28 12:36:22 +0300

    added password_test.py

--- password.py
+++ password.py
@@ -3,13 +3,12 @@ class Password:
     Password that generates new instances of passwords.
     """
 
-    contact_list = [] # Empty contact list
+    Password_locker = [] # Empty contact list
 
-    def __init__(self,username,Password):
+    def __init__(self,username,password):
 
       # docstring removed for simplicity
 
-        self.
-        self.last_name = last_name
-        self.phone_number = number
-        self.email = email
+        self.username = username
+        self.password = password
+    
