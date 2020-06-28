f9bd83344363cc82d439c2d5dbcc5295097c753e
virginiah2020 <vancyvy254@gmail.com>
2020-06-28 12:50:21 +0300

    added def setUP

--- password_test.py
+++ password_test.py
@@ -3,3 +3,9 @@ from password import Password
 
 
 class TestPassword(unittest.TestCase):
+
+     def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_password = password("virginiah2020","wamuyu")
