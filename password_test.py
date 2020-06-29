06b12dc07aeee7cbaf2a1b8b894f2a75b97db511
virginiah2020 <vancyvy254@gmail.com>
2020-06-29 05:11:46 +0300

    added a delete password comment

--- password_test.py
+++ password_test.py
@@ -23,10 +23,21 @@ class TestPassword(unittest.TestCase):
             test_save_multiple_password to check if we can save multiple password
             objects to our password_list
             '''
+            self.new_password.save_password()
+            test_password = Password("Test","password","test@gmail") # new contact
+            test_password.save_password()
+            self.assertEqual(len(Password.password_list),2)
+            #delete password
+         def test_delete_contact(self):
+            '''
+            test_delete_contact to test if we can remove a contact from our contact list
+            '''
             self.new_contact.save_contact()
             test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
             test_contact.save_contact()
-            self.assertEqual(len(Contact.contact_list),2)
+
+            self.new_contact.delete_contact()# Deleting a contact object
+            self.assertEqual(len(Contact.contact_list),1)
 
     if __name__ == '__main__':
     unittest.main()
