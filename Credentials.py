6ca8107ef44d304e49033a5e4a22de05e6830122
virginiah2020 <vancyvy254@gmail.com>
2020-06-29 12:35:39 +0300

    added credential list

--- Credentials.py
+++ Credentials.py
@@ -1,1 +1,15 @@
 import pyperclip
+class Credentials:
+
+    '''
+    class that creates instaces of user accounts
+    '''
+    credential_list = []
+
+
+    def __init__(self, username , email , passlock):
+    
+        self.username = username
+        self.email = email
+        self.passlock = passlock
+
