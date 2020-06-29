25458ff6800fac2797a1e205fccded2460fb75a1
virginiah2020 <vancyvy254@gmail.com>
2020-06-29 05:20:37 +0300

    added a delete method

--- password_test.py
+++ password_test.py
@@ -10,15 +10,15 @@ class TestPassword(unittest.TestCase):
         '''
         self.new_password = password("virginiah2020","virgy@gmail","wamuyu")
 
-         def test_init(self):
+     def test_init(self):
             '''
             test_init test case to test if the object is initialized properly
             '''
             self.assertEqual(self.new_password.username,"Virginiah2020")
             self.assertEqual(self.new_password.email,"virgy@gmail")
-            self.assertEqual(self.new)new_password.password,"wamuyu")
+            self.assertEqual(self.new_password.password,"wamuyu")
 
-         def test_save_multiple_passwords(self):
+     def test_save_multiple_passwords(self):
             '''
             test_save_multiple_password to check if we can save multiple password
             objects to our password_list
@@ -28,13 +28,18 @@ class TestPassword(unittest.TestCase):
             test_password.save_password()
             self.assertEqual(len(Password.password_list),2)
             #delete password
-         def test_delete_contact(self):
+     def test_delete_contact(self):
             '''
             test_delete_contact to test if we can remove a contact from our contact list
             '''
-            
+            self.new_password.save_password()
+            test_password = Password("Test","user","test@gmail") # new contact
+            test_password.save_password()
 
-    if __name__ == '__main__':
-    unittest.main()
+            self.new_password.delete_password()# Deleting a contact object
+            self.assertEqual(len(Password.password_list),1)
+
+     if __name__ == '__main__':
+     unittest.main()
 
 
